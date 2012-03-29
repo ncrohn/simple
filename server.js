de47ee969d04312a0dbc58622b2a92ea8b15240b
Nick Crohn <ncrohn@me.com>
2012-03-29 10:43:25 -0700

    Server now takes uploads and deploys them

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var connect = require('connect'),
     path = require('path'),
+    spawn = require('child_process').spawn,
+    fs = require('fs'),
     app, upPath;
 
 upPath = path.join(__dirname, 'upload');
@@ -9,5 +11,32 @@ app = connect()
   .use(connect.multipart({ uploadDir: upPath }))
   .use(
     function(req, res) {
-      res.end(200);
+      if(req.files.hasOwnProperty('upload')) {
+        var file = req.files.upload,
+            unzip = spawn('unzip', ['-o', '-d', path.join(__dirname, 'public'), file.path]);
+
+        unzip.stdout.on('data', function (data) {
+          console.log(data);
+        });
+
+        unzip.stderr.on('data', function (data) {
+          console.log(data);
+        });
+
+        unzip.on('exit', function (code) {
+          if(code !== 0) {
+            console.log('Something happened.');
+            res.end('failed');
+          } else {
+            console.log('Completed upload.');
+            res.end('success');
+          }
+        });
+
+        fs.rmdir(file.path,
+          function(err) {
+            if(err) console.log(err);
+          });
+
+      }
     }).listen(3055);
